c362775706957402cb1a262180145daf5719be23
Juan Gadpen <juangadpen@hotmail.com>
2022-12-27 12:09:04 -0300

    Componentes, tailwind config, NavBar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import styles from './style';
+import NavBar from './components/NavBar';
+import Hero from './components/Hero';
+import Stats from './components/Stats'
+import Business from './components/Business'
+import CardDeal from './components/CardDeal'  
+import Testimonials from './components/Testimonials'
+import Client from './components/Client'
+import CTA from './components/CTA'
+import Footer from './components/Footer'
+import Billing from './components/Billing'
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='bg-primary w-full overflow-hidden'>
+        <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+          <div className={`${styles.boxWidth}`}>
+           <NavBar />
+          </div>
+        </div>
+      
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Hero />
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Stats />
+          <Business />
+          <Billing />
+          <CardDeal />
+          <Testimonials />
+          <Client />
+          <CTA />
+          <Footer />
+        </div>    
+      </div>
     </div>
+
   );
 }
 
---
+++ src/components/index.js
@@ -0,0 +1,15 @@
+import Billing from "./Billing";
+import Business from "./Business"
+import Button from "./Button"
+import CTA from "./CTA"
+import FeedBackCard from "./FeedBackCard"
+import Footer from "./Footer"
+import GetStarted from "./GetStarted"
+import Hero from "./Hero"
+import NavBar from "./NavBar"
+import Stats from "./Stats"
+import Testimonials from "./Testimonials"
+import CardDeal from "./CardDeal"
+import Client from "./Client"
+
+export{Client,CardDeal,Billing,Business,Button,CTA,FeedBackCard,Footer,GetStarted,Hero,NavBar,Stats,Testimonials}
