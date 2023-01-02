5ed6e430523de115c9fa0bd20694b71619f90405
Juan Gadpen <juangadpen@hotmail.com>
2023-01-01 22:51:53 -0300

    Creacion y actulizacion de los componentes del sitio web

--- src/components/CardDeal.js
+++ src/components/CardDeal.js
@@ -1,9 +1,29 @@
 import React from "react";
+import { card } from "../assets";
+import styles,{layout} from "../style";
+import Button from './Button'
+
 
 export default function CardDeal(){
     return (
-        <div>
-            CardDeal
-        </div>
+        <section className={layout.section}>
+            <div className={layout.sectionInfo}>
+                <h2 className={styles.heading2}>
+                    Find a better card deal<br className="sm:block hidden"/> in few easy steps
+                </h2>
+                
+                <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque faucibus, lorem in pulvinar rutrum, metus ante pulvinar eros, at tincidunt purus turpis sit amet nisl. Praesent volutpat ullamcorper mauris, non eleifend augue feugiat ultrices. Quisque sit amet ligula sodales, feugiat libero id, aliquet justo.
+                </p>
+                <Button styles="mt-10"/>
+            </div>
+            
+            <div className={layout.sectionImg}>                
+                    <img src={card} alt="card" className="w-[100%] h-[100%]" />
+                </div>
+
+
+
+        </section>
     )
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,58 @@
 import React from "react";
+import {logo} from '../assets';
+import {footerLinks, socialMedia} from '../constants';
+import styles from '../style'
 
 export default function Footer(){
     return (
-        <div>
-            Footer
+    <section className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
+        <div className={`${styles.flexStart} md:flew-row flex-col mb-8 w-full`}>
+            <div className="flex-1 flex- flex-col justify-start mr-10 ">
+                <img src={logo} alt="hoobanck" className="w-[260px] h-[72px] object-contain"/>
+                <p className={`${styles.paragraph} mt-4 max-w-[310px]`}> A new way to make the payment easy, reliable and secure</p>
+            </div>
+
+            <div className="flex-[1.5] w-full flex flew-row justify-between flex-wrap md:mt-0 mt-10">
+                {footerLinks.map((footerLink)=>(
+                    
+                    <div key={footerLink.key} className="flex       flex-cl ss:my-0 my-4 min-w-[150px]">
+
+                        <ul> 
+
+                        <h4 className="mt-10 font-poppins font-medium text-[18px] leading-[27px] text-white">
+                            {footerLink.title}
+                        </h4>
+
+                            {footerLink.links.map((link,index)=>
+                               ( <li key={link.name} className={`list-none mt-4 font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer ${index !== footerLink.links.length -1 ? 'mb-4': 'mb-0'}`}>
+
+                                {link.name}
+
+                                </li>
+                            ))}
+                        </ul>
+                    </div>              
+                ))}
+
+            </div>
         </div>
+
+        <div className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3f3r45]">
+        <p className="font-poppins font-normal text-center  tex-[18px] leading-[27px] text-white"> 2022 HooBank, All Rights Reserved. </p>
+        </div>
+
+        <div className="flex flex-row md:mt-0 mt-6">
+            {socialMedia.map((social,index)=>(
+                <img
+                key={social.id}
+                src={social.icon}
+                alt={social.id}
+                className={`w-[21px] h-[21px] object-contain cursor-pointer ${index !== socialMedia.length -1 ? 'mr-6' : 'mr-0'}`}
+
+                />
+            ))}
+
+        </div>
+    </section>
     )
 }
